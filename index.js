42a0cd00036b28c6c8ef68cb1b6ab338a19478e3
thiagocifani <cifani.thiago@gmail.com>
2020-04-06 19:51:20 -0300

    Adds router.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const router  = require('./src/routes');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(router);
 
 app.listen(port, () => {
   console.log("Running Server...");
 })
-
-app.get("/", (req, res) => res.send("hello"));
