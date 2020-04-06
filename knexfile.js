20fd66824293365c08575170b883d8e219c84c31
thiagocifani <cifani.thiago@gmail.com>
2020-04-06 19:51:11 -0300

    Changes path for connection and migrations.

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,12 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './src/database/dev.sqlite3'
+    },
+     migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true
   },
 
   staging: {
