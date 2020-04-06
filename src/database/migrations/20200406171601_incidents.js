3769adc9f8499260bcde5d4b69fc3ed7156c901c
thiagocifani <cifani.thiago@gmail.com>
2020-04-06 19:51:42 -0300

    Moves migration to its own folder.

--- src/database/migrations/20200406171601_incidents.js
+++ src/database/migrations/20200406171601_incidents.js
@@ -3,9 +3,10 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('incidents', function (table) {
        table.increments('id');
-       table.string('name', 255).notNullable();
+       table.string('title', 255).notNullable();
        table.string('description', 255).notNullable();
        table.decimal('price').notNullable();
+       table.integer('ong_id').notNullable();
     });
 };
 
